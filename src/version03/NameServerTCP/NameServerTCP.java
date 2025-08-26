437e5b4364b64c64000462bdd2103280a31c4d52
Karsten <karsten-kirkegaard@hotmail.com>
2025-08-26 10:58:10 +0200

    Verison3 nu med mapper

--- src/version03/NameServerTCP/NameServerTCP.java
+++ src/version03/NameServerTCP/NameServerTCP.java
@@ -1,4 +1,20 @@
 package version03.NameServerTCP;
 
+import version03.ClientInfo;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.List;
+
 public class NameServerTCP {
+    private static final List<ClientInfo> clientList = new java.util.ArrayList<>();
+    private static final int PORT = 12080;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("NavneServer (TCP) kører på port " + PORT);
+
+
+
+    }
 }
