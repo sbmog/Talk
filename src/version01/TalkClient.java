f7de0da5b0acdc8e2743e342d0ef897b148050e0
sbmog <eaa24sbmog@students.eaaa.dk>
2025-08-26 09:34:56 +0200

    Version01

---
+++ src/version01/TalkClient.java
@@ -0,0 +1,33 @@
+package version01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.io.DataOutputStream;
+
+public class TalkClient {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("LocalHost", 12080);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        while (true) {
+            // clientSocket.isConnected();
+
+            String sentence = inFromUser.readLine();
+            outToServer.writeBytes(sentence + '\n');
+
+            String fromServer = inFromServer.readLine();
+            System.out.println("From other client: " + fromServer);
+        }
+
+//        clientSocket.close();
+
+    }
+}
+
