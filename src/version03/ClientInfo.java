5c9c66854aa6bb12c479d51f19fba2ddabc6ff89
Karsten <karsten-kirkegaard@hotmail.com>
2025-08-26 10:52:02 +0200

    Verison3 nu med mapper

---
+++ src/version03/ClientInfo.java
@@ -0,0 +1,13 @@
+package version03;
+
+public class ClientInfo {
+    String name;
+    String ip;
+    int port;
+
+    ClientInfo(String name, String ip, int port) {
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+    }
+}
